c263a7fcf93dcd30510f39d4721c46c6b9484bb0
Silvio Salinas Antón <ssalinas.silvio@gmail.com>
2020-08-29 00:35:34 -0600

    Creando Vista Login
    Se agregan formulario del login

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
 
 const Login = () => {
+
+    const onChange = () =>{
+
+    }
+
     return (
-        <h1>Desde Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Inicio de Sesión</h1>
+                <form action="">
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Tu Email"
+                            onChange={onChange}
+                            />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="email">Password</label>
+                        <input 
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Tu Password"
+                            onChange={onChange}
+                            />
+                    </div>
+
+                    <div className="campo-form">
+                        <input type="submit" className="btn btn=primario btn-block"
+                            value = "Iniciar Sesion"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 
