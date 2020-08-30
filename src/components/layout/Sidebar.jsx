5769db2fb599d7bf98327178c5db4f94bd7e0f9b
Silvio Salinas Antón <ssalinas.silvio@gmail.com>
2020-08-30 15:07:30 -0600

    Agregando formulario de crear nuevo proyecto

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from 'react';
+import NuevoProyecto from '../proyectos/NuevoProyecto';
 
 const Sidebar = () => {
     return (
         <aside>
             <h1>MERN <span>Task</span> </h1>
+            <NuevoProyecto></NuevoProyecto>
             <div className="proyectos">
                 <h2>Tus Proyectos</h2>
             </div>
-        
+
         </aside>
     )
 }
