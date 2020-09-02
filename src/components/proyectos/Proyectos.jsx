ef04cf15ee0a6a5f59e951ff3c3c2ee360501d3c
Silvio Salinas Antón <ssalinas.silvio@gmail.com>
2020-09-02 00:30:32 -0600

    Creando Formulario de Tareas

--- src/components/proyectos/Proyectos.jsx
+++ src/components/proyectos/Proyectos.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Sidebar from '../layout/Sidebar';
+import Header from '../layout/Header';
+import FormTarea from '../tareas/FormTarea';
+
 const Proyectos = () => {
     return (
         <div className="contenedor-app">
-            <Sidebar></Sidebar>
-            <div className="section-principal">
+            <Sidebar/>
+            <div className="seccion-principal">
+                <Header/>
                 <main>
+                    <FormTarea></FormTarea>
                     <div className="contenedor-tareas">
 
                     </div>
